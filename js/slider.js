721c9139a3d537ad1a844ae0fb1573427cf5883e
Oussemajbeli <jbelioussema33@gmail.com>
2024-02-04 17:53:09 +0100

    last

--- js/slider.js
+++ js/slider.js
@@ -6,6 +6,14 @@ let slideritem3 = document.getElementById('slider-item3');
 let slideritem4 = document.getElementById('slider-item4');
 let slideritem5 = document.getElementById('slider-item5');
 
+let disc1 = document.getElementById('disc1');
+let disc2 = document.getElementById('disc2');
+let disc3 = document.getElementById('disc3');
+let disc4 = document.getElementById('disc4');
+let disc5 = document.getElementById('disc5');
+
+let discription = document.getElementById('discription');
+
     function showSlides(index) {
       const sliderWrapper = document.getElementById('sliderWrapper');
       const totalSlides = document.querySelectorAll('.slider-item').length;
@@ -18,6 +26,12 @@ let slideritem5 = document.getElementById('slider-item5');
             slideritem3.classList.remove("hovered_slider");
             slideritem4.classList.remove("hovered_slider");
             slideritem5.classList.remove("hovered_slider");
+
+            disc1.classList.add("right_visible");
+            disc2.classList.remove("right_visible");
+            disc3.classList.remove("right_visible");
+            disc4.classList.remove("right_visible");
+            disc5.classList.remove("right_visible");
             break;
         case -1:
                 slideritem2.classList.add("hovered_slider");
@@ -25,6 +39,12 @@ let slideritem5 = document.getElementById('slider-item5');
                 slideritem3.classList.remove("hovered_slider");
                 slideritem4.classList.remove("hovered_slider");
                 slideritem5.classList.remove("hovered_slider");
+
+                disc2.classList.add("right_visible");
+                disc1.classList.remove("right_visible");
+                disc3.classList.remove("right_visible");
+                disc4.classList.remove("right_visible");
+                disc5.classList.remove("right_visible");
             break;
         case -2:
                     slideritem3.classList.add("hovered_slider");
@@ -32,6 +52,12 @@ let slideritem5 = document.getElementById('slider-item5');
                     slideritem1.classList.remove("hovered_slider");
                     slideritem4.classList.remove("hovered_slider");
                     slideritem5.classList.remove("hovered_slider");
+
+                    disc3.classList.add("right_visible");
+                    disc2.classList.remove("right_visible");
+                    disc1.classList.remove("right_visible");
+                    disc4.classList.remove("right_visible");
+                    disc5.classList.remove("right_visible");
             break;
         case -3:
                         slideritem4.classList.add("hovered_slider");
@@ -39,6 +65,12 @@ let slideritem5 = document.getElementById('slider-item5');
                         slideritem3.classList.remove("hovered_slider");
                         slideritem1.classList.remove("hovered_slider");
                         slideritem5.classList.remove("hovered_slider");
+
+                        disc4.classList.add("right_visible");
+                        disc2.classList.remove("right_visible");
+                        disc3.classList.remove("right_visible");
+                        disc1.classList.remove("right_visible");
+                        disc5.classList.remove("right_visible");
             break;
         case -4:
                             slideritem5.classList.add("hovered_slider");
@@ -46,6 +78,12 @@ let slideritem5 = document.getElementById('slider-item5');
                             slideritem3.classList.remove("hovered_slider");
                             slideritem4.classList.remove("hovered_slider");
                             slideritem1.classList.remove("hovered_slider");
+
+                            disc5.classList.add("right_visible");
+                            disc2.classList.remove("right_visible");
+                            disc3.classList.remove("right_visible");
+                            disc4.classList.remove("right_visible");
+                            disc1.classList.remove("right_visible");
         break;
       
         default:
